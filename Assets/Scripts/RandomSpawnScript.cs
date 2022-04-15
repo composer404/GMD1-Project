4332f44692a327b009ddd227b5babe11a01daf17
Estera <dumi_estera@yahoo.com>
2022-04-15 20:10:27 +0200

    Spawner improvments: no colisions and different spawn places #2

--- Assets/Scripts/RandomSpawnScript.cs
+++ Assets/Scripts/RandomSpawnScript.cs
@@ -9,34 +9,81 @@ using Random = UnityEngine.Random;
 public class RandomSpawnScript : MonoBehaviour
 {
 
- [SerializeField] private GameObject prefabObject;
- [SerializeField] private float xPos;
- [SerializeField] private float zPos;
+// [SerializeField] private GameObject prefabObject;
+//   private float xPos;
+//   private float zPos;
  [SerializeField] private int count;
  [SerializeField] private float delayTime = 2f;
+   public Transform prefabToSpawn;
+    // public int objectCount = 50;
+    public float spawnRadius = 5;
+    public float spawnCollisionCheckRadius;
 
- 
- 
+
+ public GameObject[] spawnLocations;
+ //private List<Transform> spawnSpotList;
+ //[SerializeField] private NavMeshSurface[] surfaces;
+
+// private void Awake()
+// {
+//     NavMeshDatas = new NavMeshData[surfaces.Length];
+//     for(int i=0;i<surfaces.Length; i++)
+//     {
+//         NavMeshDatas[i] = new NavMeshData();
+//         NavMesh.AddNavMeshData(NavMeshDatas[i]);
+//     }
+// BuildNavMesh(false);
+// StartCoroutine(CheckPlayerMovment());
+
+void Awake()
+{
+    spawnLocations = GameObject.FindGameObjectsWithTag("SpawnPoint");
+ }
 
  void Start()
  {
- StartCoroutine(PrefabDrop());
+  
+ StartCoroutine(PrefabDrop1());
+
  }
 
  
- IEnumerator PrefabDrop()
+//  IEnumerator PrefabDrop1()
+//  {
+//   while (count <2)
+//   {
+//    xPos = Random.Range(-30, 30);
+//    zPos = Random.Range(-23, 29);
+//    Instantiate(prefabObject, new Vector3(xPos, 0, zPos), Quaternion.identity);
+//    yield return new WaitForSeconds(delayTime);
+//    count += 1;
+
+//   }
+//  }
+
+IEnumerator PrefabDrop1()
  {
   while (count <10)
   {
-   xPos = Random.Range(-30, 30);
-   zPos = Random.Range(-23, 29);
-   Instantiate(prefabObject, new Vector3(xPos, 0, zPos), Quaternion.identity);
+       int spawn = Random.Range(0,spawnLocations.Length);
+      for(int loop = 0; loop < count;loop++)
+        {
+            Vector3 spawnPoint = transform.position + Random.insideUnitSphere*spawnRadius;
+            if(!Physics.CheckSphere(spawnPoint,spawnCollisionCheckRadius))
+            {
+                 Instantiate(prefabToSpawn, spawnLocations[spawn].transform.position, Quaternion.identity);
+            }
+            
+        }
+
    yield return new WaitForSeconds(delayTime);
    count += 1;
 
   }
  }
 }
+
+
 
  
 
